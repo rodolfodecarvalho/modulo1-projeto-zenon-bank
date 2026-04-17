1cf70b97bed9291ceae27d90dc01e5bddd5c562d
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2026-04-17 16:28:08 -0300

    feat: internacionalização report

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/service/TransactionReport.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/service/TransactionReport.java
@@ -15,43 +15,6 @@ public class TransactionReport {
 
     private static final Logger LOGGER = Logger.getLogger(TransactionReport.class.getName());
 
-    private record ReportTransaction(BigDecimal amount, Boolean isFraud) {
-    }
-
-   public record Statistics(Long totalRegister, Long totalIsFraud, BigDecimal totalAmount) {
-        private static final Statistics STATISTICS_ZERO = new Statistics(0L, 0L, BigDecimal.ZERO);
-
-        public static Statistics readTransactions(final String path) {
-            try (Stream<String> lines = lines(Paths.get(path))) {
-
-                return lines
-                        .skip(1)
-                        .filter(line -> !line.isBlank())
-                        .map(TransactionReport::mapLineToReportTransaction)
-                        .filter(Optional::isPresent)
-                        .map(Optional::get)
-                        .reduce(
-                                STATISTICS_ZERO,
-                                Statistics::getStatistics,
-                                Statistics::add
-                        );
-            } catch (Exception ex) {
-                throw new TransactionException(String.format("Erro ao ler o arquivo: %s", path), ex);
-            }
-        }
-
-       private static Statistics getStatistics(Statistics acc, ReportTransaction re) {
-           return new Statistics(
-                   acc.totalRegister + 1,
-                   acc.totalIsFraud + (Boolean.TRUE.equals(re.isFraud) ? 1 : 0),
-                   acc.totalAmount.add(re.amount));
-       }
-
-        private Statistics add(Statistics other) {
-            return new Statistics(totalRegister + other.totalRegister, totalIsFraud + other.totalIsFraud, totalAmount.add(other.totalAmount));
-        }
-    }
-
     private static Optional<ReportTransaction> mapLineToReportTransaction(final String line) {
         String[] fields = line.split(",");
 
@@ -82,6 +45,43 @@ public class TransactionReport {
             return new BigDecimal(value);
         } catch (NumberFormatException ex) {
             throw new IllegalArgumentException("Valor inválido para " + "amount" + ": " + value + " | linha: " + line, ex);
+        }
+    }
+
+    private record ReportTransaction(BigDecimal amount, Boolean isFraud) {
+    }
+
+    public record Statistics(Long totalRegister, Long totalIsFraud, BigDecimal totalAmount) {
+        private static final Statistics STATISTICS_ZERO = new Statistics(0L, 0L, BigDecimal.ZERO);
+
+        public static Statistics readTransactions(final String path) {
+            try (Stream<String> lines = lines(Paths.get(path))) {
+
+                return lines
+                        .skip(1)
+                        .filter(line -> !line.isBlank())
+                        .map(TransactionReport::mapLineToReportTransaction)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .reduce(
+                                STATISTICS_ZERO,
+                                Statistics::getStatistics,
+                                Statistics::add
+                        );
+            } catch (Exception ex) {
+                throw new TransactionException(String.format("Erro ao ler o arquivo: %s", path), ex);
+            }
+        }
+
+        private static Statistics getStatistics(Statistics acc, ReportTransaction re) {
+            return new Statistics(
+                    acc.totalRegister + 1,
+                    acc.totalIsFraud + (Boolean.TRUE.equals(re.isFraud) ? 1 : 0),
+                    acc.totalAmount.add(re.amount));
+        }
+
+        private Statistics add(Statistics other) {
+            return new Statistics(totalRegister + other.totalRegister, totalIsFraud + other.totalIsFraud, totalAmount.add(other.totalAmount));
         }
     }
 }
